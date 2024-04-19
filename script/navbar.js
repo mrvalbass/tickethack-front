3e0af896578cc95b6a48f9670df2da4e13bf0ed0
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-04-19 09:47:10 +0200

    Remove default value of user name

--- script/navbar.js
+++ script/navbar.js
@@ -2,11 +2,13 @@ const navBar = document.querySelector("nav");
 const cartLink = navBar.firstElementChild;
 const bookingsLink = cartLink.nextElementSibling;
 const userLink = navBar.querySelector("#user-status");
+const userName = userLink.querySelector("a");
 
 async function updateNavBar() {
   if (!window.localStorage.getItem("userId")) {
     cartLink.classList.add("hidden");
     bookingsLink.classList.add("hidden");
+    userName.textContent = "Sign Up / Sign In";
   } else {
     cartLink.classList.remove("hidden");
     bookingsLink.classList.remove("hidden");
@@ -17,7 +19,6 @@ async function updateNavBar() {
     ).then((r) => r.json());
     if (response.result) {
       const profilePicture = document.createElement("img");
-      const userName = userLink.querySelector("a");
       profilePicture.src = response.user.profilePictureURL;
       profilePicture.classList.add(
         "rounded-full",
